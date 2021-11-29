a6e310b97decb76a7894332751f40fd00aaf73f9
harging <gavin.hargin@gmail.com>
2021-11-29 16:43:50 +0000

    added link to all releases and some flair

--- filmreleases/src/containers/FilmReleaseSchedule.js
+++ filmreleases/src/containers/FilmReleaseSchedule.js
@@ -7,27 +7,28 @@ const FilmReleaseSchedule = () => {
     [
         {
           id: 1,
-          name: "Spider-Man: Into the Spider-Verse",
-          url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+          name: "Resident Evil: Welcome to Raccoon City",
+          url: "https://www.imdb.com/title/tt6920084/?ref_=rlm"
         },
         {
           id: 2,
-          name: "Life Itself",
-          url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+          name: "Silent Night",
+          url: "https://www.imdb.com/title/tt11628854/?ref_=rlm"
         },
         {
           id: 3,
-          name: "Mary Queen of Scots",
-          url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+          name: "C'mon C'mon",
+          url: "https://www.imdb.com/title/tt10986222/?ref_=rlm"
         },
         {
           id: 4,
-          name: "The Lego Movie 2: The Second Part", url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+          name: "Boxing Day", 
+          url: "https://www.imdb.com/title/tt13659918/?ref_=rlm"
         },
         {
           id: 5,
-          name: "Captain Marvel",
-          url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+          name: "Blue Bayou",
+          url: "https://www.imdb.com/title/tt11121664/?ref_=rlm"
         }
       ]
   )
@@ -35,7 +36,10 @@ const FilmReleaseSchedule = () => {
   return (
     <>
       <h1>Upcoming Film Releases for UK</h1>
+      <hr></hr>
       <FilmReleases films={films} />
+      <hr></hr>
+      <a href="https://www.imdb.com/calendar/?region=gb"><h1>View more upcoming releases >>></h1></a>
     </>
   );
 
